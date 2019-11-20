51f5269e01ccfb8abc1d72f73c36175df283ce01
chnirt <trinhchinchin@gmail.com>
2019-11-20 08:27:53 +0000

    Fix

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "../styles/index.css";
+
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
