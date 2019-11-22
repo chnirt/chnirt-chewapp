9c796e246237758cdb41687c259f63bfe4b5dd71
chnirt <trinhchinchin@gmail.com>
2019-11-22 08:33:31 +0000

    Fix route

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var app = express();
 app.use(express.static(path.join(__dirname, "dist")));
 app.set("port", process.env.PORT || 14044);
 
+app.get("*", (request, response) => {
+  response.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 var server = app.listen(app.get("port"), function() {
   console.log("listening on port ", server.address().port);
 });
