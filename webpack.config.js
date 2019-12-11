0e9103ff73c2707f53ed73c59b43ad7f663440b9
chnirt <trinhchinchin@gmail.com>
2019-12-11 13:52:11 +0700

    Fix

--- webpack.config.js
+++ webpack.config.js
@@ -37,19 +37,19 @@ module.exports = {
 			{
 				test: /\.css$/,
 				use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-						options: {
-							publicPath: (resourcePath, context) => {
-								// publicPath is the relative path of the resource to the context
-								// e.g. for ./css/admin/main.css the publicPath will be ../../
-								// while for ./css/main.css the publicPath will be ../
-								return path.relative(path.dirname(resourcePath), context) + '/'
-							},
-							hmr: process.env.NODE_ENV === 'development',
-							reloadAll: true
-						}
-					},
+					// {
+					// 	loader: MiniCssExtractPlugin.loader,
+					// 	options: {
+					// 		publicPath: (resourcePath, context) => {
+					// 			// publicPath is the relative path of the resource to the context
+					// 			// e.g. for ./css/admin/main.css the publicPath will be ../../
+					// 			// while for ./css/main.css the publicPath will be ../
+					// 			return path.relative(path.dirname(resourcePath), context) + '/'
+					// 		},
+					// 		hmr: process.env.NODE_ENV === 'development',
+					// 		reloadAll: true
+					// 	}
+					// },
 					'css-loader'
 				]
 			},
